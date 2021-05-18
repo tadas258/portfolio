1cc4198d82e6581ac591bf3e4c37bf1228d81d33
tadas258 <t.jackevicius19@gmail.com>
2021-05-19 00:10:16 +0300

    resume_2

--- js/components/skill.js
+++ js/components/skill.js
@@ -1,24 +1,32 @@
 function showUpWindow() {
-const clc = document.querySelector(".cancel");
-const left_container = document.querySelector(".left_container");
+  const clc = document.querySelector(".leftCancel");
+  const left_container = document.querySelector(".left_container");
+  const clcr = document.querySelector(".rightCancel");
+  const right_container = document.querySelector(".right_container");
 
-clc.addEventListener("click", ()=> {
-    
-
-    
-    if(left_container.classList.contains("off")) {
-        left_container.classList.remove("off");
-        left_container.classList.add("active");
-     
-        
+  clc.addEventListener("click", () => {
+    if (left_container.classList.contains("leftOff")) {
+      left_container.classList.remove("leftOff");
+      left_container.classList.add("leftActive");
+      document.querySelector(".rightCancel").style.zIndex = "-1";
     } else {
-        left_container.classList.add("off");
-        left_container.classList.remove("active");
-    
-
+      left_container.classList.add("leftOff");
+      left_container.classList.remove("leftActive");
+      document.querySelector(".rightCancel").style.zIndex = "1";
     }
-});
+  });
+
+  clcr.addEventListener("click", () => {
+    if (right_container.classList.contains("rightOff")) {
+      right_container.classList.remove("rightOff");
+      right_container.classList.add("rightActive");
+      document.querySelector(".leftCancel").style.zIndex = "-1";
+    } else {
+      right_container.classList.add("rightOff");
+      right_container.classList.remove("rightActive");
+      document.querySelector(".leftCancel").style.zIndex = "1";
+    }
+  });
 }
 
-
-export {showUpWindow};
+export { showUpWindow };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import {showUpWindow} from './components/skill.js';
-import {socialsData} from './data/socialsData.js';
-import {renderSocials} from './components/renderSocials.js';
+import { showUpWindow } from "./components/skill.js";
+// import { showUpWindowRight } from "./components/skill.js";
+import { socialsData } from "./data/socialsData.js";
+import { renderSocials } from "./components/renderSocials.js";
 
-renderSocials('.socials', socialsData);
+renderSocials(".socials", socialsData);
 showUpWindow();
+// showUpWindowRight();
